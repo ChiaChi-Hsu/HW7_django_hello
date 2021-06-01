288e8facd75825c6e8729e938825c545faa11f7a
ChiaChi-Hsu <plus7kiki926@gmail.com>
2021-06-01 14:23:28 +0800

    Fourth Commit

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -10,14 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-
-from pathlib import Path
-
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -29,7 +26,6 @@ SECRET_KEY = 'django-insecure-zpq4=6dm6rspwr96e$%l6oku5s8$)##zh6qc*^)xguk-&gobxx
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','myhello20210601.herokuapp.com']
-
 
 # Application definition
 
@@ -74,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hello_django.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -84,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -103,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
@@ -150,4 +143,3 @@ LOGGING = {
         },   
     },
 }
-
